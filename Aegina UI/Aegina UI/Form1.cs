b1649817598cc620b76a56f1f47c3227b83c9ef7
Jackbail4 <64487175+Jackbail4@users.noreply.github.com>
2022-01-17 00:15:15 +0000

    Update Form1.cs

--- Aegina UI/Aegina UI/Form1.cs
+++ Aegina UI/Aegina UI/Form1.cs
@@ -1,9 +1,10 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
+using System.Diagnostics;
 using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
